6bf37a713c4c2029f2112462cccc319f655c7fd6
bemmons1993 <61093072+bemmons1993@users.noreply.github.com>
2020-11-12 15:05:27 -0600

    clean up spacing

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ app.use(bodyParser.text());
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
-
-
-
-
-
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
